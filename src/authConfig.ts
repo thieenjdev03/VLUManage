97cdbdf5ebde5542ce7a83f27a0b0c441a60f73d
Thien GKIM PC <thien@gkim.vn>
2025-01-04 18:21:29 +0700

    Change auth config

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,25 +1,24 @@
-import { Configuration } from "@azure/msal-browser";
+import { Configuration } from '@azure/msal-browser';
 
-const redirectUriLocal = 'http://localhost:3039/login'
-const redirectUriSTAG = import.meta.env.VITE_REDIRECT_URI_LOGIN_STAGING;
+const redirectUriLocal = 'http://localhost:3039/login';
 const redirectUriPROD = import.meta.env.VITE_REDIRECT_URI_LOGIN_PROD;
 
-const _redirectUri =
-  window.location.href.includes("localhost")
-    ? redirectUriLocal :  window.location.href.includes("staging") ? redirectUriSTAG : redirectUriPROD;
+const _redirectUri = window.location.href.includes('localhost')
+  ? redirectUriLocal
+  : redirectUriPROD;
 
 console.log('redirectUri', _redirectUri);
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "c6e8b39f-9e08-4ef1-86a2-0475a7c2160a",
-    authority: "https://login.microsoftonline.com/common", 
+    clientId: 'c6e8b39f-9e08-4ef1-86a2-0475a7c2160a',
+    authority: 'https://login.microsoftonline.com/common',
     redirectUri: _redirectUri,
   },
   cache: {
-    cacheLocation: "localStorage",
-    storeAuthStateInCookie: true, 
+    cacheLocation: 'localStorage',
+    storeAuthStateInCookie: true,
   },
 };
 export const loginRequest = {
-  scopes: ["User.Read"], 
+  scopes: ['User.Read'],
 };
